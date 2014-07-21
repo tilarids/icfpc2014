1fad69def30ac580291fc4f6e130156aadadc4ba
Andy Melnikov <andy.melnikov@gmail.com>
2014-07-21 22:06:32 +0300

    sausage 25x121

--- pixelSausage5.hs
+++ pixelSausage5.hs
@@ -8,14 +8,19 @@ import qualified Data.Map as M
 xdecode :: BL.ByteString -> Maybe [[Int]]
 xdecode x = decode x
 
-sausage size = show . concatMap (chunksOf size) 
+sausage size = show . chunksOf size . concat
 
 
 main = do
 	x <- BL.readFile "bkg-2-decimated.json"
 	let bitLL = fromJust $ xdecode x
-
+	
 	writeFile "sausage5.json" $ sausage 5 bitLL
 	writeFile "sausage5-transposed.json" $ sausage 5 $ transpose bitLL
+	writeFile "sausage11.json" $ sausage 11 bitLL
+	writeFile "sausage11-transposed.json" $ sausage 11 $ transpose bitLL
+	writeFile "sausage25.json" $ sausage 25 bitLL
+	writeFile "sausage25-transposed.json" $ sausage 25 $ transpose bitLL
+
 
 
