bf9e99ae2dffb2137213d0b9da7737c824d0f042
dkopiychenko <kopiychenko@gmail.com>
2014-07-26 00:56:13 +0300

    Emulator draft

---
+++ java/src/app/GhostAI.java
@@ -0,0 +1,22 @@
+package app;
+
+/**
+ * Created by dkopiychenko on 7/25/14.
+ */
+public class GhostAI {
+
+    public GhostAI() {
+    }
+
+    public Object run(Cons map, VM.Tuple<Integer,Integer> location, Integer direction) {
+        int x = location.a;
+        int y = location.b;
+
+        return null;
+    }
+
+    public int run(Cons map) {
+
+        return 0;
+    }
+}
---
+++ java/src/app/ManAI.java
@@ -0,0 +1,22 @@
+package app;
+
+/**
+ * Created by dkopiychenko on 7/25/14.
+ */
+public class ManAI {
+
+    public ManAI() {
+    }
+
+    public int run(Cons map, VM.Tuple<Integer,Integer> location, Integer direction, Integer lives, Integer score) {
+        int x = location.a;
+        int y = location.b;
+
+        return 0;
+    }
+
+    public int run(Cons map) {
+
+        return 0;
+    }
+}
