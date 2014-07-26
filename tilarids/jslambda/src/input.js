8d63feedbb3dc6d2df3d9611b779ecf5360bd267
Sergey Kishchenko <voidwrk@gmail.com>
2014-07-25 17:51:29 -0700

    input done

--- tilarids/jslambda/src/input.js
+++ tilarids/jslambda/src/input.js
@@ -5,16 +5,17 @@
     return foldl(func, func(acc, car(list)), cdr(list))
   } 
 }*/
-function mod(x, y) {
+/*function mod(x, y) {
   if (x > y) {
     return mod(x - y, y)
   } else {
     return x
   }
 }
-/*function step(state, world, lm_status, ghost_status, fruit_status) {
-  return [mod(state / 100, 4), state + 1]; 
-}
+
+function step(state, world, lm_status, ghost_status, fruit_status) {
+  return [0, state + 1]; 
+}/*
 function queue_new() {
   return [0, 0];
 }
@@ -32,7 +33,7 @@ function queue_isempty(q) {
     return 0;
   }
 }*/
-function reverse(c) {
+/*function reverse(c) {
   if (atom(c)) {
     return c;
   } else {
@@ -51,13 +52,22 @@ function queue_dequeue(q) {
     return [car(car(q)), cdr(car(q)), cdr(q)];
   }
 }
-
+*/
 //function id(x, y) { return x; } 
 /*function step_impl(state, tuple4) { 
   return step(state, car(tuple4), car(cdr(tuple4)), car(cdr(cdr(tuple4))), cdr(cdr(cdr(tuple4))))
 }*/
+function imperative_step(state, x11, x12, x21, x22) {
+  return [0, [x11, x12, x21, x22]];
+}
+function new_step(state, world) {
+  return imperative_step(state, car(car(world)), car(cdr(car(world))), car(cdr(world)), car(cdr(cdr(world))))
+} 
+function new_step_impl(state, tuple4) {
+  return new_step(state, car(tuple4))
+}
 function init() {
 //  return 42;
-  return mod(7,3)
-//    return [0, step_impl]
+//  return mod(154,3)
+  return [0, new_step_impl]
 }
