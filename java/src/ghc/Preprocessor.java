c2030f1339e0272a6c2e6e863fa29e034929a654
svg <sergey.grishaev@gmail.com>
2014-07-28 00:06:44 +0300

    Allow parameter

--- java/src/ghc/Preprocessor.java
+++ java/src/ghc/Preprocessor.java
@@ -24,7 +24,7 @@ public class Preprocessor {
 
         List<String> prog = new ArrayList<>();
         try {
-            FileInputStream fis = new FileInputStream("src/ghc/hunter.ghc_src");
+            FileInputStream fis = new FileInputStream(args.length == 0 ? "src/ghc/example.ghc" : args[0]);
             BufferedReader br = new BufferedReader(new InputStreamReader(fis));
 
             String strLine;
